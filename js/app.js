56c1b6ecffbb7cbb98be2eb14552fc0f5bc9eace
Sunny-Sama <13302010096@fudan.edu.cn>
2018-07-17 19:52:57 +0800

    initial version

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Sunny on 2018/7/17.
+ */
+var myApp = angular.module('myApp', ['ui.router', 'myApp.controllers']);
+myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when('', '/main');
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            templateUrl: 'templates/login.html',
+            controller: 'loginCtrl'
+        })
+
+        .state('admin', {
+            url: '/admin',
+            templateUrl: 'templates/admin.html',
+            controller: 'adminCtrl'
+        })
+
+        .state('main', {
+            url: 'main',
+            templateUrl: 'templates/main.html',
+            controller: 'mainCtrl'
+        })
+}]);
---
+++ js/controllers.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Sunny on 2018/7/17.
+ */
+angular.module('myApp.controllers', [])
+.controller('loginCtrl', function($scope, $rootScope, $http){
+
+})
+.controller('adminCtrl', function () {
+    
+})
+.controller('mainCtrl',function () {
+
+})
